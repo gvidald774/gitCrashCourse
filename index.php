8ca680bb4f96ca9e81117bcfba1bbc276201e061
Guillermo Vidal Aldarias <gvidald774@g.educaand.es>
2021-10-05 12:10:04 +0200

    Añadida versión inicial de la página

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Título</title>
+</head>
+<body>
+    <h2>Calculadora de tu edad más cuatro años</h2>
+    <p>Próximamente...</p>
+</body>
+</html>
